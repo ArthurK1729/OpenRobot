1134bf8657b0544537e78173526a5f590f08ae4b
Arthur <a.krochin@hotmail.com>
2018-08-04 22:05:53 +0100

    Add graph checker yaml

--- preprocessor_graph/common/PreprocessorCheckAbstractClass.py
+++ preprocessor_graph/common/PreprocessorCheckAbstractClass.py
@@ -1,10 +1,22 @@
 from abc import ABC, abstractmethod
+from typing import Type, List
+import pandas as pd
+from preprocessing.common.PrintablePreprocessorAbstractClass import PrintablePreprocessorAbstractClass
+
 
 class PreprocessorCheckAbstractClass(ABC):
     """
     Every preprocessing checker must implement this abstract class
     """
 
+    def __init__(self, parent_node, child_nodes):
+        self.parent_node: Type[PreprocessorCheckAbstractClass] = parent_node
+        self.child_nodes: List[Type[PreprocessorCheckAbstractClass]] = child_nodes
+
     @abstractmethod
-    def check(self, df_column) -> bool:
+    def check(self, df_column: Type[pd.Series]) -> bool:
+        pass
+
+    @abstractmethod
+    def get_preprocessor(self) -> Type[PrintablePreprocessorAbstractClass]:
         pass
